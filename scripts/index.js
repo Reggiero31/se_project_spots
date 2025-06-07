c75f584a5e334b5ac0e059f5c1f5e97dd6407f75
Reginald Robinson <reggierobinson012@gmail.com>
2025-06-07 12:07:46 -0500

    se_project_spots

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,23 @@
 const editProfileBtn = document.querySelector(".profile__edit-btn");
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseBtn = document.querySelector(".modal__close-btn");
+const editprofileForm = document.querySelector(".modal__form");
+const editprofilenameInput = document.querySelector("#profile-name-input");
+
+const editprofileDescriptionInput = document.querySelector(
+  "#profile-description-input"
+);
 
 const NewPostBtn = document.querySelector(".profile__add-btn");
 const newpostmodal = document.querySelector("#new-post-modal");
 const NewPostCloseBtn = newpostmodal.querySelector(".modal__close-btn");
 
+const profilenameEl = document.querySelector(".profile__name");
+const profiledescriptionEl = document.querySelector(".profile__description");
+
 editProfileBtn.addEventListener("click", function () {
+  editprofilenameInput.value = profilenameEl.textcontent;
+
   editProfileModal.classList.add("modal_is-opened");
 });
 
@@ -21,3 +32,9 @@ NewPostBtn.addEventListener("click", function () {
 NewPostCloseBtn.addEventListener("click", function () {
   newpostmodal.classList.remove("modal_is-opened");
 });
+
+function handleeditprofilesubmit(evt) {
+  evt.preventdefault();
+  profilenameEl.textcontent = editprofilenameinput.value;
+}
+editprofileform.addEventListener("submit", handleeditprofilesubmit);

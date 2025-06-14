f9d0e6b44f18ea6acc90c776fce02f672110e716
Reginald Robinson <reggierobinson012@gmail.com>
2025-06-14 18:20:50 -0500

    se_project_spots

--- scripts/index.js
+++ scripts/index.js
@@ -8,16 +8,18 @@ const editprofileDescriptionInput = document.querySelector(
   "#profile-description-input"
 );
 
-const NewPostBtn = document.querySelector(".profile__add-btn");
+const newpostbtn = document.querySelector(".profile__add-btn");
 const newpostmodal = document.querySelector("#new-post-modal");
-const NewPostCloseBtn = newpostmodal.querySelector(".modal__close-btn");
-
+const newpostclosebtn = newpostmodal.querySelector(".modal__close-btn");
+const newPostForm = newpostmodal.querySelector(".modal__form");
+const newPostCaption = newPostForm.querySelector("#card-description-input");
+const newPostLink = newPostForm.querySelector("#card-image-input");
 const profilenameEl = document.querySelector(".profile__name");
 const profiledescriptionEl = document.querySelector(".profile__description");
 
 editProfileBtn.addEventListener("click", function () {
-  editprofilenameInput.value = profilenameEl.textcontent;
-
+  editprofilenameInput.value = profilenameEl.textContent;
+  editprofileDescriptionInput.value = profiledescriptionEl.textContent;
   editProfileModal.classList.add("modal_is-opened");
 });
 
@@ -25,16 +27,23 @@ editProfileCloseBtn.addEventListener("click", function () {
   editProfileModal.classList.remove("modal_is-opened");
 });
 
-NewPostBtn.addEventListener("click", function () {
+newpostbtn.addEventListener("click", function () {
   newpostmodal.classList.add("modal_is-opened");
 });
 
-NewPostCloseBtn.addEventListener("click", function () {
+newpostclosebtn.addEventListener("click", function () {
   newpostmodal.classList.remove("modal_is-opened");
 });
 
 function handleeditprofilesubmit(evt) {
-  evt.preventdefault();
-  profilenameEl.textcontent = editprofilenameinput.value;
+  evt.preventDefault();
+  profilenameEl.textContent = editprofilenameInput.value;
+  profiledescriptionEl.textContent = editprofileDescriptionInput.value;
 }
-editprofileform.addEventListener("submit", handleeditprofilesubmit);
+editprofileForm.addEventListener("submit", handleeditprofilesubmit);
+newPostForm.addEventListener("submit", function (evt) {
+  evt.preventDefault();
+  console.log(newPostCaption.value);
+  console.log(newPostLink.value);
+  newpostmodal.classList.remove("modal_is-opened");
+});
